669ea8c84c55d573092614b085856fa4e32d394b
Ryter <gmtg2009@gmail.com>
2018-06-07 16:14:04 +0300

    add slice

---
+++ webpack.config.js
@@ -0,0 +1,118 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageMinPlugin = require('imagemin-webpack-plugin');
+
+const extractHTML = new ExtractTextPlugin('index.html');
+const extractCSS = new ExtractTextPlugin('css/style.css');
+
+const conf = {
+  //context: path.resolve(__dirname, 'dist'),
+
+  entry: [
+    './src/js/app.js',
+    './src/slim/index.slim',
+    './src/scss/style.scss'
+  ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/build.js',
+  },
+
+  devServer: {
+    overlay: true
+  },
+
+  module: {
+    rules: [
+        {
+          test: /\.slim$/,
+          use: extractHTML.extract({
+            use: [
+              {
+                loader: 'html-loader',
+                options: {
+                  minimize: false
+                }
+              },{
+                loader: 'slim-lang-loader',
+                options: {
+                  slimOptions: {
+                    //'pretty': true
+                  }
+                }
+              }
+            ]
+        })
+      },{
+        test: /\.scss$/,
+        use: extractCSS.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {sourceMap: true}
+            },{
+              loader: 'postcss-loader',
+              options: {sourceMap: true}
+            },{
+              loader: 'sass-loader',
+              options: {sourceMap: true}
+            }
+          ],
+          fallback: 'style-loader'
+        })
+      },{
+        test: /\.(png|gif|jpe?g)$/,
+        loaders: [
+            {
+              //loader: 'file-loader?name=images/[name].[ext]',
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: './images/',
+                //publicPath: '../images/'
+              },
+          },
+          'img-loader',
+        ]
+      },{
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              encoding: 'base64'
+            }
+          },{
+            loader: 'svgo-loader',
+            options: {
+              plugins: [
+                {
+                  removeViewBox: false
+                }
+              ]
+            }
+          },
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    extractHTML,
+    extractCSS,
+    new CopyWebpackPlugin (
+      [
+        {from: './src/images', to: 'images'}
+      ], {
+        ignore : [
+          {glob: 'svg/*'}
+        ]
+      }
+    )
+  ]
+};
+
+module.exports = conf;
